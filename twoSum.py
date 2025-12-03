cb38d774261b0e610867bd59145fcd57cc5c2483
rajitakolla <rajitakolla@gmail.com>
2025-12-03 00:52:07 -0500

    Add files via upload

---
+++ twoSum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = []
+
+        for i in range(len(nums)):
+            diff = target-nums[i]
+            if diff in nums[i+1:]:
+                j = nums[i+1:].index(diff)
+                result = [i,j+i+1]
+                break
+        return result
+
+    # time complexity : o(n^2)
+    #space complexity : o(1)
