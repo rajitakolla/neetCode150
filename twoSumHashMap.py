62f45c0955c033f7f6cee483ce36b43226a5d843
rajitakolla <rajitakolla@gmail.com>
2025-12-03 01:00:14 -0500

    Add files via upload

---
+++ twoSumHashMap.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}
+
+        for i, n in enumerate(nums):
+            indices[n] = i
+
+        for i, n in enumerate(nums):
+            diff = target - n
+
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+
+        return []
+
+# Time complexity : o(n)
+# space complexity: o(n)
