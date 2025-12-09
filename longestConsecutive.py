3e510e110913a42f453b28e6e79f193dfc2901b5
rajitakolla <rajitakolla@gmail.com>
2025-12-09 13:56:18 -0500

    Add files via upload

---
+++ longestConsecutive.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
+        numset = set(nums)
+        result = 0
+
+        for val in numset:
+            if (val-1) not in numset:
+                temp = 1
+                while (val+temp) in numset:
+                    temp += 1
+                result = max(result, temp)
+
+        return result
+
+
+# Time complexity : o(n)
+# space complexity : o(n)
