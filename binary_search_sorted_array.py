0b4a37b2ba1136f4303886746c723e90c570b6ae
rajitakolla <rajitakolla@gmail.com>
2026-01-10 16:35:06 -0500

    Add files via upload

---
+++ binary_search_sorted_array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        top, bottom = 0, n
+
+        required_row = 0
+
+        if target > matrix[m-1][n-1]:
+            return False
+
+        while top <= bottom:
+            if matrix[top][n-1] < target:
+                top += 1
+            else:
+                required_row = top
+                break
+        start, end = 0, n-1
+
+        while start <= end:
+            mid = (start+end)//2
+
+            if matrix[required_row][mid] < target:
+                start = mid+1
+            elif matrix[required_row][mid] > target:
+                end = mid-1
+            else:
+                return True
+        return False
+
+
+# Time complexity : 0(m+logn)
+#space complexity :0(1)
