0dcbece24bd4205f01f472a50fbe11d3e160e89f
rajitakolla <rajitakolla@gmail.com>
2026-01-10 20:50:20 -0500

    Add files via upload

---
+++ minElementInRotatedArray.py
@@ -0,0 +1,25 @@
+# https://neetcode.io/problems/find-minimum-in-rotated-sorted-array/question
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        left, right = 0, len(nums)-1
+        result = nums[0]
+
+        while left <= right:
+            
+            if nums[left] < nums[right]:
+                result = min(result, nums[left])
+                break
+
+            mid = (left+right)//2
+            result = min(result, nums[mid])
+
+            if nums[mid] >= nums[left]:
+                left = mid+1
+            else:
+                right = mid-1
+        return result
+
+
+# Time Complexity: o(log n)
+# Space complexity: o(1)
